3409f091dccb3a903f980b8b330b8c2ae01f8524
swetha-11-srisai <swethachowdary857@gmail.com>
2021-11-21 19:10:19 -0800

    [Swetha] add : Linked list created

---
+++ DataStructure/DataStructure/LinkedList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure
+{
+    class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next!=null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} insert into linked list:",node.data);
+        }
+     
+    }
+}
